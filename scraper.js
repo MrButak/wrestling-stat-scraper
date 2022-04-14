46d421ff2a1cfd29bb5f96a19ceae66b1016fb5f
Matthew Spence <mspence5555@gmail.com>
2022-04-13 20:44:35 -0500

    refactoring code and breaking it down into smaller functions

--- scraper.js
+++ scraper.js
@@ -1,7 +1,16 @@
 const puppeteer = require('puppeteer');
 config = require('dotenv').config()
 
+const { Pool, Client } = require('pg')
 
+const client = new Client({
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+        rejectUnauthorized: false
+    }
+});
+
+client.connect();
 
 
 async function getFirstData() {
@@ -23,14 +32,15 @@ async function getFirstData() {
             page.click('.form-signin .btn'),
             page.waitForNavigation(),
         ]);
-
-        let urls = ['https://www.wrestlestat.com/season/2022/team/1/air-force/profile',
-                    'https://www.wrestlestat.com/season/2022/team/2/american/profile'];
+        let years = ['2022', '2020', '2018', '2016']
+        let schools = ['air-force', 'american', 'appalachian-state', 'arizona-state']
+        
+        
 
         for(let i = 0; i < 1; i++) {
             
             // navigate to schedule page
-            URL = urls[i];
+            URL = `https://www.wrestlestat.com/season/${years[0]}/team/${schools.indexOf(schools[0]) + 1}/${schools[0]}/profile`;
             
             await page.goto(URL, {
                 waitUntil: 'load',
@@ -64,7 +74,86 @@ async function getFirstData() {
                 return data;
             });
 
-            parseFirstData(rawData);
+            // build object from raw data
+            let tmpStr = '';
+            let tmpCnt = 1;
+            let tmpArry = [];
+            let tmpObj = {};
+
+            for(let i = 0; i < rawData.length; i++) {
+
+                tmpStr = '';
+                tmpArry = [];
+
+                for(let j = 0; j < rawData[i].values.length; j++) {
+
+                    if(rawData[i].values[j].trim() == '') {continue};
+                    tmpStr = rawData[i].values[j].trim();
+                    tmpArry.push(tmpStr)
+                };
+
+                tmpObj[tmpCnt] = tmpArry;
+                tmpCnt++;
+                
+            };
+            
+            
+            let tmpPointsArry = [];
+            let scheduleObj = {
+                type: [],
+                date: [],
+                eventName: [],
+                points: [],
+                oppName: []
+            }
+    
+            Object.keys(tmpObj).forEach((key) => {
+                
+                
+                scheduleObj.date.push(tmpObj[key][0].split('\n')[0]);
+                scheduleObj.type.push(tmpObj[key][1].split('\n')[0]);
+
+                switch(tmpObj[key].length) {
+
+                    case 5:
+                        scheduleObj.eventName.push(tmpObj[key][2]);
+                        scheduleObj.oppName.push(null);
+                        scheduleObj.points.push(null);
+                        scheduleObj.points.push(null);
+                        break;
+                    case 6:
+                        scheduleObj.eventName.push(null);
+                        scheduleObj.oppName.push(tmpObj[key][3].substring(tmpObj[key][3].indexOf(' ') + 1));
+                        // determine what index score goes in by W or L
+                        if(tmpObj[key][4].toLowerCase().includes('w')) {
+
+                            if(tmpObj[key][5].split(' - ')[0] < tmpObj[key][5].split(' - ')[1]) {
+                                tmpPointsArry.unshift(tmpObj[key][5].split(' - ')[0]);
+                                tmpPointsArry.unshift(tmpObj[key][5].split(' - ')[1]);
+                            }
+                            else {
+                                tmpPointsArry.push(tmpObj[key][5].split(' - ')[0]);
+                                tmpPointsArry.push(tmpObj[key][5].split(' - ')[1]);
+                            }
+                        }
+                        else {
+                            if(tmpObj[key][5].split(' - ')[0] < tmpObj[key][5].split(' - ')[1]) {
+                                tmpPointsArry.push(tmpObj[key][5].split(' - ')[0]);
+                                tmpPointsArry.push(tmpObj[key][5].split(' - ')[1]);
+                            }
+                            else {
+                                tmpPointsArry.unshift(tmpObj[key][5].split(' - ')[0]);
+                                tmpPointsArry.unshift(tmpObj[key][5].split(' - ')[1]);
+                            }
+                        };
+                        scheduleObj.points.push(tmpPointsArry)
+                        
+                };
+
+                tmpPointsArry = [];
+                
+            });
+            console.log(scheduleObj);
             await page.evaluate(() => window.stop());
         };
         
@@ -76,138 +165,25 @@ async function getFirstData() {
     };
 };
 
-let parseFirstData = (rawData) => {
 
-    let tmpStr = '';
-    let tmpCnt = 1;
-    let inCnt = 0;
-    let tmpArry = [];
-    let tmpObj = {};
+// async function insertInDb()  {
 
-    for(let i = 0; i < rawData.length; i++) {
-
-        inCnt = 0;
-        tmpStr = '';
-        tmpArry = [];
-
-        for(let j = 0; j < rawData[i].values.length; j++) {
-
-            if(rawData[i].values[j].includes('unofficial') || rawData[i].values[j].includes('Big 12 9th Place Matches')) {inCnt++};  
-            if(rawData[i].values[j].trim() == '') {continue};
-            tmpStr = rawData[i].values[j].trim();
-            tmpArry.push(tmpStr)
-        };
-
-        if(inCnt > 0) {continue};
-        tmpObj[tmpCnt] = tmpArry;
-        tmpCnt++;
-        
-    };
-    buildScheduleObjOne(tmpObj);
-    return;
-    
-};
-
-let buildScheduleObjOne = (rawObj) => {
-
-    let tmpPointsArry = [];
-    let scheduleObj = {
-        type: [],
-        date: [],
-        eventName: [],
-        points: [],
-        oppName: []
-    }
-    
-    Object.keys(rawObj).forEach((key) => {
-        
-        
-        scheduleObj.date.push(rawObj[key][0].split('\n')[0]);
-        scheduleObj.type.push(rawObj[key][1].split('\n')[0]);
-
-        switch(rawObj[key].length) {
-
-            case 5:
-                scheduleObj.eventName.push(rawObj[key][2]);
-                scheduleObj.oppName.push(null);
-                scheduleObj.points.push(null);
-                scheduleObj.points.push(null);
-                break;
-            case 6:
-                scheduleObj.eventName.push(null);
-                scheduleObj.oppName.push(rawObj[key][3].substring(rawObj[key][3].indexOf(' ') + 1));
-                tmpPointsArry.push(rawObj[key][5].split(' - ')[0]);
-                tmpPointsArry.push(rawObj[key][5].split(' - ')[1]);
-                scheduleObj.points.push(tmpPointsArry)
-                
-        };
-
-        tmpPointsArry = [];
-        
-    });
-
-    console.log(scheduleObj);
-    return;
-};
-
-// async function getFirstData() {
+//     const text = 'INSERT INTO purchases(stripe_pi, email, items_purchased, total_price, shipping_address, account_id) VALUES($1, $2, $3, $4, $5, $6) RETURNING *';
+//     const values = [stripePiId, email, itemsPurchased, totalPrice, shippingAddress, null];
 
 //     try {
-
-//         let URL = 'https://goduke.com/sports/wrestling/schedule/2021-22?grid=true';
-
-//         // navigate to schedule page
-//         let browser = await puppeteer.launch({headless: false});
-//         let page = await browser.newPage();
-        
-//         await page.goto(URL, {
-//             waitUntil: 'load',
-//             timeout: 0
-//         });
-//         await page.goto(URL);
-        
-//         // block loading of resources like images and css
-//         await page.setRequestInterception(true);
-        
-//         // get table data
-//         let rawData = await page.evaluate(() => {
-
-//             let data = [];
-//             let table = document.querySelector('table')
-
-//             for (var i = 1; i < table.rows.length; i++) {
-
-//                 let objCells = table.rows.item(i).cells;
-
-//                 let values = [];
-//                 for (var j = 0; j < objCells.length; j++) {
-//                     // if I choose to use .innerHTML here, I need to remove all <span> and other tags with .remove()
-//                     let text = objCells.item(j).textContent;
-//                     values.push(text);
-//                 };
-
-//                 let d = { i, values };
-//                 data.push(d);
-//             };
-
-//             return data;
-//         });
-
-//         iterate(rawData)
-        
-//         await browser.close();
-//     }
-
-//     catch(err) {
-//         console.log(err);
+//         const res = await client.query(text, values)
+//     } 
+//     catch (error) {
+//         console.log(error.stack)
 //     };
 // };
-
 
 
 async function main() {
 
     getFirstData();
+    // getSecondData();
 };
 
 main();
--- tmp.js
+++ tmp.js
@@ -1,3 +1,7 @@
-let myStr = '#5 Oklahoma State'
+// let myStr = '#5 Oklahoma State'
 
-console.log(myStr.substring(myStr.indexOf(' ') + 1)); // "tocirah sneab"
+// console.log(myStr.substring(myStr.indexOf(' ') + 1)); // Oklahoma State
+
+let tempArry = ['bla', 'blee', 'blay', 'blue'];
+console.log(`Hey there ${tempArry[0]}`)
+console.log(tempArry.indexOf(tempArry[0]) + 1)
